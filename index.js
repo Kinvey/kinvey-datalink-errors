1010b52076a0570c66c1ff176aab95e6abebcde6
mjsalinger <mjsalinger@gmail.com>
2018-06-29 15:44:09 -0400

    Remove coffeescript register

--- index.js
+++ index.js
@@ -10,6 +10,4 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 
-require('coffee-script');
-require('coffee-script/register');
 module.exports = require('./lib/errors');
